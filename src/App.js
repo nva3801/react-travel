964cce0cc4341fc65a331427721934af2f6748ce
nva3801 <nva030801@gmail.com>
2022-06-12 21:08:31 +0700

    souce

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
+import CategoryPage from "./pages/CategoryPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
         <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+        <Route path="/category" element={<CategoryPage></CategoryPage>}></Route>
+        <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <div className="h-[100vh] flex items-center justify-center flex-col">
+      <div className="max-w-[1000px] mx-auto text-center">
+        <NavLink to="/">
+          <img src="logo.png" alt="" className="w-[50%] mx-auto" />
+        </NavLink>
+        <p className="text-[60px] py-10 font-bold text-center">
+          Oops! Page not found!
+        </p>
+        <NavLink to="/" className="max-w-[120px]">
+          <button className="text-center bg-primary text-white text-3xl py-4 rounded-lg w-[250px] mx-auto">
+            Back to home
+          </button>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
