17f2e9f918ae090180c7339f23f7a24b2a7f8b80
nva3801 <nva030801@gmail.com>
2022-06-27 20:37:19 +0700

    source

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -11,31 +11,44 @@ const Header = () => {
     }
   };
   window.addEventListener("scroll", changeBackground);
-
+  let [open, setOpen] = useState(false);
+  console.log(open);
   return (
     <header
       className={
         header
           ? "fixed bg-white top-0 w-full leading-[60px] h-[60px] transition-all z-50 border border-b-2 justify-between"
-          : "bg-transparent w-full h-[110px] fixed top-0 z-10 leading-[110px] transition-all text-white z-50"
+          : "bg-transparent w-full h-[110px] fixed top-0 z-10 leading-[110px] transition-all text-white z-50 md:leading-[60px] md:h-[60px] "
       }
     >
-      <div className="container">
-        <div className="flex items-center justify-between h-full md:mt-3">
-          <NavLink to="/" className="max-h-[60px] w-[120px]">
+      <div className="container md:p-0">
+        <div className="flex items-center justify-between h-full md:mt-3 md:block">
+          <NavLink to="/" className="max-h-[60px] w-[120px] ">
             <img
               src="/logo.png"
               alt=""
-              className={header ? "object-cover w-full h-full block" : "hidden"}
+              className={
+                header
+                  ? "object-cover w-full h-full block md:ml-3 md:h-[40px] md:w-[120px] md:object-cover"
+                  : "hidden"
+              }
             />
             <img
               src="/logo-white.png"
               alt=""
-              className={header ? "object-cover w-full h-full hidden" : "block"}
+              className={
+                header
+                  ? "hidden"
+                  : "block w-full h-full object-cover md:h-[40px] md:w-[120px] md:object-cover md:ml-3"
+              }
             />
           </NavLink>
-          <div className="sm:hidden">
-            <ul className="flex font-bold uppercase gap-x-8 ">
+          <div
+            className={`relative md:text-black md:bg-white md:px-4 md:pb-4 md:${
+              open ? "" : "hidden"
+            }`}
+          >
+            <ul className="flex gap-8 font-bold uppercase md:block md:gap-2 md:flex-col">
               <li>
                 <a
                   href=""
@@ -60,9 +73,9 @@ const Header = () => {
                   <div
                     className={`absolute flex w-[520px] grid-cols-3 leading-8 text-black bg-white gap-x-5 px-5 py-3 rounded-lg ${
                       header ? "top-[100%]" : "top-[80%]"
-                    }`}
+                    } md:w-full md:grid-cols-1 md:block md:p-0 md:relative`}
                   >
-                    <div className="flex flex-col w-1/3">
+                    <div className="flex flex-col w-1/3 md:w-full">
                       <NavLink to="/category">Du lịch miền bắc</NavLink>
                       <a
                         href=""
@@ -83,7 +96,7 @@ const Header = () => {
                         Du lịch hạ long
                       </a>
                     </div>
-                    <div className="flex flex-col w-1/3">
+                    <div className="flex flex-col w-1/3 md:w-full">
                       <a href="" className="">
                         Du lịch miền trung
                       </a>
@@ -101,7 +114,7 @@ const Header = () => {
                         Du lịch phú yên
                       </a>
                     </div>
-                    <div className="flex flex-col w-1/3 ">
+                    <div className="flex flex-col w-1/3 md:w-full">
                       <a href="" className="">
                         Du lịch miền nam
                       </a>
@@ -129,9 +142,9 @@ const Header = () => {
                   <div
                     className={`absolute flex w-[520px] grid-cols-3 leading-8 text-black bg-white gap-x-5 px-5 py-3 rounded-lg ${
                       header ? "top-[100%]" : "top-[80%]"
-                    }`}
+                    } md:w-full md:grid-cols-1 md:block md:p-0 md:relative`}
                   >
-                    <div className="flex flex-col w-1/3">
+                    <div className="flex flex-col w-1/3 md:w-full">
                       <a href="" className="">
                         Du lịch châu á
                       </a>
@@ -148,7 +161,7 @@ const Header = () => {
                         Du lịch nhật bản
                       </a>
                     </div>
-                    <div className="flex flex-col w-1/3">
+                    <div className="flex flex-col w-1/3 md:w-full">
                       <a href="" className="">
                         Du lịch châu âu
                       </a>
@@ -165,7 +178,7 @@ const Header = () => {
                         Du lịch nga
                       </a>
                     </div>
-                    <div className="flex flex-col w-1/3 ">
+                    <div className="flex flex-col w-1/3 md:w-full">
                       <a href="" className="">
                         Du lịch châu mỹ
                       </a>
@@ -203,7 +216,7 @@ const Header = () => {
               </li>
               <li
                 className={`bg-primary p-1 h-[32px] ${
-                  header ? "mt-[13px]" : "mt-[38px]"
+                  header ? "mt-[13px]" : "mt-[38px] md:mt-[20px]"
                 } rounded-lg`}
               >
                 <span>
@@ -224,6 +237,25 @@ const Header = () => {
                 </span>
               </li>
             </ul>
+          </div>
+          <div
+            className="absolute hidden px-2 py-1 text-black bg-white rounded-sm right-4 top-3 md:block"
+            onClick={() => setOpen(!open)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
           </div>
         </div>
       </div>
--- src/modules/Home/Slider.jsx
+++ src/modules/Home/Slider.jsx
@@ -4,13 +4,17 @@ const Slider = () => {
   return (
     <section className="slider">
       <div className="relative w-full h-full">
-        <img src="/slide.jpg" alt="" className="object-cover w-full h-full" />
+        <img
+          src="/slide.jpg"
+          alt=""
+          className="object-cover w-full h-full md:h-[500px]"
+        />
         <div className="text-white absolute bottom-[20%] w-full">
           <div className="container">
-            <span className="uppercase text-lg leading-[60px]">
+            <span className="uppercase text-lg leading-[60px] md:text-md md:leading-none">
               mona travel - đi và trải nghiệm mọi thứ
             </span>
-            <h1 className="text-[46px] leading-[55px] font-bold uppercase">
+            <h1 className="text-[46px] leading-[55px] font-bold uppercase md:text-xl">
               đồng hành cùng bạn
               <br /> mọi nẻo đường
             </h1>
